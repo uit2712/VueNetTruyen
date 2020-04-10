b9ed346170e3a8017fcb446d3680f87c337f6516
uit2712 <phamquangvi456@gmail.com>
2020-04-10 10:58:59 +0700

    temp

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,29 @@
     <div id="app">
         <Header />
         <TopMenu />
+        <div class="main-content row d-flex justify-content-center">
+            <RecommendComic/>
+        </div>
     </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import TopMenu from "./components/TopMenu.vue";
+import RecommendComic from "./components/RecommendComic.vue";
 
 export default {
     name: "App",
     components: {
         Header,
-        TopMenu
+        TopMenu,
+        RecommendComic,
     }
 };
 </script>
 
-<style>
+<style scoped>
+.main-content {
+    width: 75%;
+}
 </style>
